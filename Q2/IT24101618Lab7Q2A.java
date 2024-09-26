b1016378e7a253a9dafa8e2e55b7fc48d7ea56c2
IT24101618 <IT24101618@my.sliit.lk>
2024-09-26 22:15:25 +0530

    Add files via upload

---
+++ Q2/IT24101618Lab7Q2A.java
@@ -0,0 +1,17 @@
+
+
+public class IT24101618Lab7Q2A{
+    public static void main(String[] args) {
+       
+        int rows = 4;
+        int columns =5;
+        
+        for( int columncount = 0;columncount<columns;columncount++){
+            for(int rowcount = 0;rowcount< rows;rowcount++){
+                System.out.print("$ ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
